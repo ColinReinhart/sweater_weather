a991d3a00f91ca39a716dc6abb34395296588382
Colin Reinhart <colinbreinhartgmail.com>
2022-08-07 14:19:01 -0600

    Remove unwanted hourly data

--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -23,10 +23,30 @@ class Weather
     @visibility = attributes[:current][:visibility]
     @conditions = attributes[:current][:weather].first[:description]
     @icon = attributes[:current][:weather].first[:icon]
+    @hourly_weather = remove_hour_data(attributes[:hourly][0..7])
+    @daily_weather = attributes[:daily][0..4]
+    require "pry"; binding.pry
   end
 
   def date_format(utc)
     date = Time.zone.at(utc).strftime('%b %e, %l:%M %p')
+  end
+
+  def remove_hour_data(data)
+    data.each do |hour|
+      hour.delete(:feels_like)
+      hour.delete(:pressure)
+      hour.delete(:humidity)
+      hour.delete(:dew_point)
+      hour.delete(:uvi)
+      hour.delete(:clouds)
+      hour.delete(:visibility)
+      hour.delete(:wind_speed)
+      hour.delete(:wind_deg)
+      hour.delete(:wind_gust)
+      hour.delete(:pop)
+      conditions[:hour][:weather][:id][:description].first
+    end
   end
 
 end
