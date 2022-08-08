685ce96a93e1a0d8a8a78d3bbe032806f053bb80
Colin Reinhart <colinbreinhartgmail.com>
2022-08-08 08:59:44 -0600

    add location vars

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ForecastController < ApplicationController
 
   def index
     location = LocationFacade.get_location(params[:location])
-    weather = WeatherFacade.get_weather(location)
+    weather = WeatherFacade.get_weather(location.lat, location.lon)
   end
 
 end
