d5e2fd622c9540d69e70b46d380663ebf13d93c1
Colin Reinhart <colinbreinhartgmail.com>
2022-08-09 17:55:19 -0600

    Roadtrip

--- app/poros/direction.rb
+++ app/poros/direction.rb
@@ -1,9 +1,11 @@
 class Direction
   attr_reader :travel_hours,
               :formatted_time
+              
 
   def initialize(argument)
     @formatted_time = argument[:route][:formattedTime]
     @travel_hours = argument[:route][:formattedTime].to_i
+
   end
 end
