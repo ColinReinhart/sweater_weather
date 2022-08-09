e6f57e0d7a0075a50d5633587b669ac37079e4bd
Colin Reinhart <colinbreinhartgmail.com>
2022-08-09 10:47:31 -0600

    user registration sad path

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,31 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :check_fields
 
-def create
-  user_create = User.new(user_params)
-  user_create[:api_key] = SecureRandom.hex
-  if user_create.save
-    # session[:id] = user_create[:id]
-    render json: UserSerializer.new(user_create), status: 201
-  else
-    render json: [errors: 'invalid credentials']
-  end
-end
-
-private
-  def user_params
-    params.permit(:email, :password, :password_confirmation)
+  def create
+    user_create = User.new(user_params)
+    user_create[:api_key] = SecureRandom.hex
+    if user_create.save
+      # session[:id] = user_create[:id]
+      render json: UserSerializer.new(user_create), status: 201
+    else
+      render json: [errors: 'invalid credentials'], status: 401
+    end
   end
 
+  private
+    def user_params
+      params.permit(:email, :password, :password_confirmation)
+    end
+
+    def check_fields
+      if user_params[:email] == ""
+        render json: { error: "Email Required To Register"}, status: 401
+      elsif user_params[:password] == ""
+        render json: { error: "Password Required To Register"}, status: 401
+      elsif user_params[:password_confirmation] == ""
+        render json: { error: "Password Confirmation Required To Register"}, status: 401
+      elseif user_params[:password] != user_params[:password_confirmation]
+        render json: { error: "Password Does Not Match Confirmation"}, status: 401
+      end
+  end
 end
