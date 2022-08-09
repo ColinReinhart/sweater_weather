9d284c2301eb978af8cf98a78da255a37b3529a5
Colin Reinhart <colinbreinhartgmail.com>
2022-08-09 10:55:03 -0600

    Full coverage

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,8 +7,6 @@ class Api::V1::UsersController < ApplicationController
     if user_create.save
       # session[:id] = user_create[:id]
       render json: UserSerializer.new(user_create), status: 201
-    else
-      render json: [errors: 'invalid credentials'], status: 401
     end
   end
 
